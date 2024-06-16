4989934ae2100d123a75b14353ae71d7056df573
98emre <emre-98@live.se>
2024-06-16 16:05:59 +0200

    fix: name change and extra properties added

--- MachineManagement.Core/Dtos/DeviceDtos/DeviceWithoutItemDto.cs
+++ MachineManagement.Core/Dtos/DeviceDtos/DeviceWithoutItemDto.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace MachineManagement.Core.Dtos.Device
 {
-    public class DeviceWithIdDto
+    public class DeviceWithoutItemDto
     {
         public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public bool Status { get; set; }
+
+        public DateOnly Date { get; set; }
     }
 }
