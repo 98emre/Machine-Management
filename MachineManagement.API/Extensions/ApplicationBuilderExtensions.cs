7ad073ff4af75e449cd2dad1213d2d8b79d9352a
98emre <emre-98@live.se>
2024-06-16 21:31:06 +0200

    feat: added extensions

---
+++ MachineManagement.API/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using MachineManagement.Data.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MachineManagement.API.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<MachineManagementAPIContext>();
+
+                try
+                {
+                    await context.Database.EnsureDeletedAsync();
+                    await context.Database.MigrateAsync();
+
+                    await SeedData.InitAsync(context);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    throw;
+                }
+            }
+
+        }
+    }
+}
--- MachineManagement.API/Program.cs
+++ MachineManagement.API/Program.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MachineManagement.Data.Data;
+using MachineManagement.API.Extensions;
 
 namespace MachineManagement.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<MachineManagementAPIContext>(options =>
@@ -24,6 +25,7 @@ namespace MachineManagement.API
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                //await app.SeedDataAsync();
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
