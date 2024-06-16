6d52b3a61e11e6f226be80439ae7d43e4eb9e344
98emre <emre-98@live.se>
2024-06-16 16:09:25 +0200

    feat: added  repositoreis for device

---
+++ MachineManagement.Core/Repositories/IDeviceRepository.cs
@@ -0,0 +1,19 @@
+﻿using MachineManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineManagement.Core.Repositories
+{
+    public interface IDeviceRepository
+    {
+        Task<IEnumerable<Device>> GetAllAsync();
+        Task<Device> GetAsync(int id, bool includeGames);
+        Task<bool> AnyAsync(int id);
+        void Add(Device device);
+        void Update(Device device);
+        void Remove(Device device);
+    }
+}
