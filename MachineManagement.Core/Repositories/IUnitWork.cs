c0bf5c20a733b220b7b1864f9cd8cfd3f5379f38
98emre <emre-98@live.se>
2024-06-16 16:17:26 +0200

    feat: added unitwork

---
+++ MachineManagement.Core/Repositories/IUnitWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineManagement.Core.Repositories
+{
+    public interface IUnitWork
+    {
+        IDeviceRepository DeviceRepository { get; }
+        IItemRepository ItemRepository { get; }
+
+        Task CompleteAsync();
+    }
+}
