729362cc82083a66bc3a699f00773e485aa5a79e
98emre <emre-98@live.se>
2024-06-16 15:57:17 +0200

    feat: created entities

---
+++ MachineManagement.Core/Entities/Device.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineManagement.Core.Entities
+{
+    public class Device
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public bool Status { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public ICollection<Item> Items { get; set; } = new List<Item>();
+    }
+}
---
+++ MachineManagement.Core/Entities/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineManagement.Core.Entities
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public double Price { get; set; }
+
+        [ForeignKey("DeviceId")]
+        public int DeviceId { get; set; }
+
+        public Device? Device { get; set; }
+    }
+}
