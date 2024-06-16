61c682f5c4b79f7c23ce8aa2b8a069080a5615e9
98emre <emre-98@live.se>
2024-06-16 15:57:31 +0200

    feat: added dtos for device

---
+++ MachineManagement.Core/Dtos/Device/DeviceDto.cs
@@ -0,0 +1,22 @@
+﻿using MachineManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineManagement.Core.Dtos.Device
+{
+    public class DeviceDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+
+        public bool Status { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public ICollection<Item> Items { get; set; } = new List<Item>();
+    }
+}
---
+++ MachineManagement.Core/Dtos/Device/DevicePostDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineManagement.Core.Dtos.Device
+{
+    public class DevicePostDto
+    {
+
+        [Required]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 100")]
+        public string Name { get; set; } = default!;
+
+        [Required]
+        public bool Status { get; set; }
+
+        [Required]
+        public DateOnly Date { get; set; }
+    }
+}
