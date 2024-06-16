d7e5fab464097d6cdf862998a36982868d3dc4db
98emre <emre-98@live.se>
2024-06-16 22:28:04 +0200

    feat: added unit of work logic

---
+++ MachineManagement.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using MachineManagement.Core.Repositories;
+using MachineManagement.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineManagement.Data.Repositories
+{
+    public class UnitOfWork : IUnitWork
+    {
+        private readonly MachineManagementAPIContext _context;
+
+        public UnitOfWork(MachineManagementAPIContext context)
+        {
+            _context = context;
+        }
+
+        public IDeviceRepository DeviceRepository => new DeviceRepository(_context);
+
+        public IItemRepository ItemRepository => new ItemRepository(_context);
+
+        public async Task CompleteAsync() => await _context.SaveChangesAsync();
+        
+    }
+}
