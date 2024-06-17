6575564eead6cf01d177a203f8d32e4c33bcca8c
98emre <emre-98@live.se>
2024-06-17 12:21:52 +0200

    feat: added device post modell for frontend

---
+++ Machine-Management.Frontend/Models/DevicePost.cs
@@ -0,0 +1,11 @@
+﻿namespace MachineManagement.Frontend.Models
+{
+    public class DevicePost
+    {
+        public required string Name { get; set; }
+
+        public bool Status { get; set; }
+
+        public required DateOnly Date { get; set; }
+    }
+}
